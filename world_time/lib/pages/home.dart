e6426690d85f833ea251fda2699fa0fd43ad0879
NickC <nn840928@gmail.com>
2023-06-11 21:30:45 +0800

    change loading to home page

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -8,21 +8,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context)?.settings.arguments as Map;
+    print(data);
+
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              TextButton.icon(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/location');
-                },
-                icon: Icon(Icons.edit_location),
-                label: Text('Edit Location'),
-              )
-            ],
-          ),
+        child: Column(
+          children: [
+            TextButton.icon(
+              onPressed: (){
+                Navigator.pushNamed(context, '/location');
+              },
+              icon: Icon(Icons.edit_location),
+              label: Text('Edit Location'),
+            )
+          ],
+        ),
       ),
     );
   }
