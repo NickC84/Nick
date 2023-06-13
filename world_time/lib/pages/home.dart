0fe711cb4080cae0c5fd59ab510ba354479e563d
NickC <nn840928@gmail.com>
2023-06-14 04:13:08 +0800

    finish worldTime project

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -14,21 +14,76 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
 
-    data = ModalRoute.of(context)?.settings.arguments as Map;
+    data = data.isNotEmpty? data : ModalRoute.of(context)?.settings.arguments as Map;
     print(data);
 
+    //set background
+    String bgImage = data['isDaytime'] ? 'day.png' : 'night.png';
+    var bgColor = data['isDaytime'] ? Colors.blue[200] : Colors.blue[900];
+    var textColor = data['isDaytime'] ? Colors.black : Colors.white;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            TextButton.icon(
-              onPressed: (){
-                Navigator.pushNamed(context, '/location');
-              },
-              icon: Icon(Icons.edit_location),
-              label: Text('Edit Location'),
-            )
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: [
+                TextButton.icon(
+                  onPressed: () async{
+                    dynamic result = await Navigator.pushNamed(context, '/location');
+                    setState(() {
+                      data = {
+                        'location': result['location'],
+                        'flag': result['flag'],
+                        'time': result['time'],
+                        'isDaytime' : result['isDaytime'],
+                      };
+                    });
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      data['location'],
+                      style: TextStyle(
+                        color: textColor,
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  data['time'],
+                  style: TextStyle(
+                    color: textColor,
+                    fontSize: 66.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
